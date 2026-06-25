634dd8a28a099f0ac65a7a7e766aa93960b47170
JS Deepak <jsdeepak1245@gmail.com>
2026-06-25 13:20:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Hamburger Button
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
